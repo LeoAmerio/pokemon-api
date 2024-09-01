c7a7a8afb00fd1627c4d7f1e72b3a51c9387a29c
LeoAmerio14 <leoamerio14@gmail.com>
2024-09-01 12:48:10 -0300

    battle

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-// import { PokemonModule } from './pokemon/pokemon.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { Pokemon } from './entities/pokemon.entity';
 import { BattleResult } from './entities/battle.entity';
+import { PokemonController } from './controlers/pokemon.controller';
+import { PokemonService } from './services/pokemon.service';
 
 @Module({
   imports: [
@@ -14,10 +16,10 @@ import { BattleResult } from './entities/battle.entity';
       entities: [__dirname + '/**/*.entity{.ts}'],
       synchronize: false
     }),
-    TypeOrmModule.forFeature([Pokemon, BattleResult])
-    // PokemonModule
+    TypeOrmModule.forFeature([Pokemon, BattleResult]),
+    PokemonModule
   ],
-  controllers: [AppController],
-  providers: [AppService]
+  controllers: [AppController, PokemonController],
+  providers: [AppService, PokemonService]
 })
 export class AppModule {}
--- src/controlers/battle.controller.ts
+++ src/controlers/battle.controller.ts
@@ -1,11 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { BattleService } from 'src/services/battle.service';
 
+@ApiTags('pokemon')
 @Controller('battle')
 export class BattleController {
   constructor(private readonly battleService: BattleService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Make 2 pokemons in a epic battle' })
+  @ApiResponse({ status: 200, description: 'Return the result of the fight.' })
   async battle(@Body() battleData: { pokemon1Id: number; pokemon2Id: number }) {
     const result = await this.battleService.battle(battleData.pokemon1Id, battleData.pokemon2Id);
     return {
